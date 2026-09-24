f3d0f401c570f41ebf273d10c955772fa119259e
Rodrigo Ingles <socials@velhosdorestelo.com>
2026-09-24 22:29:18 +0100

    cleanup: footers and name changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+// src/consts.ts
+export const SITE_TITLE = 'Velhos do Restelo';
+export const SITE_DESCRIPTION = 'O sítio do Podcast dos Velhos do Restelo.';
